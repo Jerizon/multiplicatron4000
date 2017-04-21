6b05e0f6f15882aeddf130309eaf4dbdfd9dd27d
Jerizon <JesseLowellReyes@gmail.com>
2017-04-21 11:35:00 -0700

    add skeleton for view management

--- client/app.js
+++ client/app.js
@@ -1,3 +1,25 @@
 const multiply = require('./multiplier.js')
 
-console.log(multiply([4, 4, 4]));
+class ViewManager {
+	connectEventHandlers() {
+		// wire up event handler for form submit
+	}
+	onSubmit(event) {
+
+		//block form from submitting
+
+		// grab number values as strings
+		
+		//cast the strings to ints
+		
+		//add ints to an array
+		
+		//reduce the array by multiplying values
+
+		//output
+	}
+
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
