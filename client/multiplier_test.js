c02389f3f535fb594184ad9c78f47fc8cbe6b96b
Jerizon <JesseLowellReyes@gmail.com>
2017-04-21 10:21:11 -0700

    Add barebones test kit

--- client/multiplier_test.js
+++ client/multiplier_test.js
@@ -1,6 +1,36 @@
 const multiply = require('./multiplier.js')
 
-console.log(multiply([2,3, 6])=== 36, ' Multiply multiple numbers');
-console.log(multiply([2,3,0])=== 0, ' Multiply by zero');
-console.log(multiply([2]) === 2, 'Multiply a single value');
-console.log(multiply([2, -2, -2, -2]) === -16, 'Multiply by multiple negative values');
+class TestSuit {
+    runTest() {
+      this.testMultiplyMultipleValues();
+      this.testMultiplyByZero();
+      this.testMultiplyASingleValue();
+      this.testMultiplyByMultipleNegativeValues();
+    }
+
+    assertEquals(a, b) {
+        return a === b;
+    }
+
+    testMultiplyMultipleValues() {
+        const result = this.assertEquals(multiply([2, 3, 6]), 36);
+        console.log(result, "testMultiplyMultipleValues");
+    }
+
+    testMultiplyByZero () {
+    	const result = this.assertEquals(multiply([2, 3, 0]), 0);
+    	console.log(result, "testMultiplyByZero");
+    }
+    testMultiplyASingleValue() {
+    	const result = this.assertEquals(multiply([2]),2);
+    	console.log(result, "testMultiplyASingleValue");
+    }
+    testMultiplyByMultipleNegativeValues() {
+    	const result = this.assertEquals(multiply([2, -2, -2, -2]), -16);
+    	console.log(result, "testMultiplyByMultipleNegativeValues");
+    }
+
+}
+
+const testSuit = new TestSuit();
+testSuit.runTest();
