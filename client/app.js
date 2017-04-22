86bc8f8393906e85fdca1e773e03a955b1293e2d
Jerizon <JesseLowellReyes@gmail.com>
2017-04-21 21:32:21 -0700

    Beautified and Stylized with CSS

--- client/app.js
+++ client/app.js
@@ -1,36 +1,44 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const multiply = require('./multiplier.js')
 
-const server = http.createServer(function(request, response) {
+class ViewManager {
 
-    const urlPath = url.parse(request.url).pathname;
-    let filePath = `./client/${urlPath}`;
+    connectEventHandlers() {
+        document.getElementById('form-numbers')
+            .addEventListener(
+                'submit',
+                this.onSubmit.bind(this));
 
-    fs.stat(filePath, function(err, fileInfo) {
-        if (!err && fileInfo.isDirectory()) {
-            filePath += '/index.html';
-        }
+        document.getElementById("addFactor").onclick = function() { addInput() }
+    }
 
-        fs.exists(filePath, function(doesExist) {
-            if (!doesExist) {
-                response.statusCode = 404;
-                response.end(`Resource not found: "${urlPath}"`);
-            } else {
-                fs.readFile(filePath, (err, data) => {
-                    if (err) {
-                        response.statusCode = 500;
-                        response.end(`Server error: "{err}"`);
-                    } else {
-                        response.end(data.toString('utf-8'));
-                    }
-                });
-            }
+    onSubmit(event) {
+        event.preventDefault();
+
+        var multiplicands = document.getElementsByClassName('input-num');
+        var hTMLValues = Array.prototype.slice.call(multiplicands);
+        var values = hTMLValues.map(function(value) {
+            return parseInt(value.value, 10);
+        })
+        values = values.filter(function(value) {
+            return !isNaN(value)
         });
+        const product = multiply(values);
+        if (isNaN(product) || values.length === 0) {
+            this.renderProduct(0);
+        } else {
+            this.renderProduct(product);
+        }
+    }
+    renderProduct(product) {
+        document.querySelector('.product').textContent = product;
+    }
+}
 
-    });
-});
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, function() {
-    console.log(`Server listening on port ${PORT}...`);
-});
+function addInput() {
+    var input = document.createElement("div");
+    input.innerHTML = '<input class="input-num" type="text" autocomplete="off"/>';
+    document.querySelector('.product').before(input);
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
--- server/server.js
+++ server/server.js
@@ -1,15 +1,36 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const server = http.createServer(function(request, response) {
 
-    fs.readFile('./client/multiplier.html', (err, data) => {
-        response.end(data.toString('UTF-8'));
+    const urlPath = url.parse(request.url).pathname;
+    let filePath = `./client/${urlPath}`;
+
+    fs.stat(filePath, function(err, fileInfo) {
+        if (!err && fileInfo.isDirectory()) {
+            filePath += '/index.html';
+        }
+
+        fs.exists(filePath, function(doesExist) {
+            if (!doesExist) {
+                response.statusCode = 404;
+                response.end(`Resource not found: "${urlPath}"`);
+            } else {
+                fs.readFile(filePath, (err, data) => {
+                    if (err) {
+                        response.statusCode = 500;
+                        response.end(`Server error: "{err}"`);
+                    } else {
+                        response.end(data.toString('utf-8'));
+                    }
+                });
+            }
+        });
+
     });
-
-    response.end('Hello World\n');
 });
-
-server.listen(3000, function() {
-    console.log('Server listening...');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function() {
+    console.log(`Server listening on port ${PORT}...`);
 });
